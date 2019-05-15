87239561f8da988ff6ff264438b5fc20e4a66d2d
tina-fernan <tinaaroom@gmail.com>
2019-05-15 10:09:32 +0200

    addTaskManager

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.scss']
 })
 export class TaskComponent implements OnInit {
+  taskName: string;
+  taskPercentage: number;
+  description: string;
 
   constructor() { }
-
   ngOnInit() {
   }
+
+  addTask() {
+    console.log(this.taskName);
+  }
+
+  addFormListener() {
+    console.log(this.taskName,this.taskPercentage,this.description);
+    
+
+  }  
 
 }
